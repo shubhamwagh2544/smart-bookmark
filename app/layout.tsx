3667da6aa18b1dd68f6b5e3e9cc6d423ebe87fcd
Shubh <shubhamwagh2544@gmail.com>
2026-02-17 12:31:19 +0530

    nextjs supabase setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,8 +9,8 @@ const defaultUrl = process.env.VERCEL_URL
 
 export const metadata: Metadata = {
   metadataBase: new URL(defaultUrl),
-  title: "Next.js and Supabase Starter Kit",
-  description: "The fastest way to build apps with Next.js and Supabase",
+  title: "Smart Bookmark",
+  description: "Fast way to add bookmarks for users",
 };
 
 const geistSans = Geist({
